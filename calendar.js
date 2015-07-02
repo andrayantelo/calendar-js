2d7c2846740f71683132b1f1fc37e25ed0b2d5d2
Andrea Anaya <andrayantelo@gmail.com>
2015-07-02 14:55:20 -0500

    cleaning up code in js file

--- calendar.js
+++ calendar.js
@@ -1,6 +1,7 @@
  /*   alert("this works");  */
  var $cal = $(".calendar")
 
+/* might not need createMonth in the end 
 var createMonth = function() {
     
     console.log("this is happening"); // check if it's working
@@ -18,7 +19,7 @@ var createMonth = function() {
         
         };
     };
-    
+*/    
     
 
 
@@ -30,54 +31,7 @@ $(document).ready(function() {
         $( this ).children().toggleClass("hidden");
        })
     
-    createMonth();
        
 });
 
 
-/* var CALENDAR = function() {
-    
-    var wrap, label, 
-        months = ["January", "February", "March", "April", "May", "June", 
-                  "July", "August", "September", "October", "November", 
-                  "December"];
-                  
-    function init(newWrap) {
-        wrap = $(newWrap || "#calendar");
-        label = wrap.find("#label");
-        wrap.find("#prev").bind("click.calendar", function () { switchMonth(false);});
-        wrap.find("#next").bind("click.calendar", function () { switchMonth(true);});
-        label.bind("click", function() {switchMonth(null, new Date().getMonth(), new Date().getFullYear()); });
-        label.click()
-        
-    }
-    
-    function switchMonth(next, month, year) {
-        
-        var curr = label.text().trim().split(" "), calendar, tempYear = parseInt(curr[1], 10);
-        month = month || ((next) ? ( (curr[0] === "December") ? 0: months.indexOf(curr[0]) + 1) : ( (curr[0] ==="January") ? 11: months.indexOf(curr[0]) - 1 ));
-        year = year || ((next && month === 0) ? tempYear + 1 : (!next && month === 11) ? tempYear - 1 : tempYear);
-        
-    }
-    
-    function createCal(year, month) {
-        $('#calendar-frame', wrap)
-            .find('.curr')
-                .removeClass('curr')
-                .addClass('temp')
-            .end()
-            .prepend(calendar.calendar())
-            .find('temp')
-                .fadeOut('slow', function() { $(this).remove(); });
-            
-        $('#label').text(calendar.label);
-        
-    }
-    createCal.cache = {};
-    return {
-        init: init,
-        switchMonth: switchMonth;
-        createCal: createCal;
-    };
-};
-*/
