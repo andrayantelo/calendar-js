187b5e57a5dabc27555e7342b2a2a4b904ac618f
Andrea Anaya <andrayantelo@gmail.com>
2015-08-18 16:50:01 -0500

    wrote methods to isolate the div type of the tds

--- calendar.js
+++ calendar.js
@@ -22,7 +22,8 @@ var createMonth = function() {
 */    
     
 
-
+var $week = $('.week');  //this is the class for the rows of the table
+var $month = $('.month');  //this is the class for the month tables
 
 $(document).ready(function() {
 
@@ -60,15 +61,15 @@ var storeInLocalStorage = function(storageItemKey, storageItem) {
         //(storageString) to it 
 };
 
-var loadFromLocalStorage = function(storageItemKey, substituteLoadedItem) {  
+var loadFromLocalStorage = function(storageItemKey, substituteLoadItem) {  
         //loads item from localstorage with key storageItemKey and returns the item
-        //if the item is not in localStorage, returns substituteLoadedItem
+        //if the item is not in localStorage, returns substituteLoadItem
         var storageItem = localStorage.getItem(storageItemKey)
         
         
         if (storageItem === null) {
             console.log(storageItemKey + "not found in localstorage");
-            return substituteLoadedItem;   
+            return substituteLoadItem;   
         }
                                                                                                    
  
@@ -81,16 +82,55 @@ var loadFromLocalStorage = function(storageItemKey, substituteLoadedItem) {
          
 };
 
-var storeMonth = function() {
-    
-};
 
-var collectCellStates = function(theTable) {
-    //goes through the rows of a table and stores the tds of the table in $tds
-    theTable.find('tr').each(function () {
-        var $tds = $(this).find('td');
+var collectDayChildrenFromMonth = function(theMonthTable) {
+    //goes through the rows of a table and stores the children of the tds of the table in $tds
+    theMonthTable.find('tr').each(function () {
+        var $tds = $(this).find('td').children();
         console.log($tds);
     })
     
     
 };
+
+var collectDayChildrenFromRow = function(theWeekRow) {
+    //goes through the rows ad collects the children of the tds
+    theWeekRow.find('td').each(function() {
+        var $tdChildren = $(this).children();
+        console.log($tdChildren);
+        console.log($tdChildren.length + " this is the length of tdChildren");
+        console.log(typeof($tdChildren) + " this is the type of tdChildren");
+    })
+};
+
+var collectDayContentFromRow = function(theWeekRow) {
+    //goes through the rows and collects the content() of the tds
+    var tdContentsArray = [];
+    console.log(tdContentsArray);
+    console.log(typeof(tdContentsArray) + " this is the before type of the contents array");
+    console.log(tdContentsArray.length + " this is the before length of the contents array");
+    theWeekRow.find('td').each(function() {
+        var $tdContents = $(this).contents();
+        //console.log($tdContents);
+        //console.log(typeof($tdContents) + " this is the type of tdContent");
+        //console.log($tdContents.length + " this is the length of tdContent");
+        tdContentsArray.push($tdContents);
+    })
+    console.log(tdContentsArray);
+    console.log(typeof(tdContentsArray) + " this is the after type of the contents array");
+    console.log(tdContentsArray.length + " this is the after length of the contents array");
+    return tdContentsArray;
+};
+var storeMonth = function() {
+    
+};
+
+//isolate the div types from tdContentsArray
+//var arrayOfDivTypes = [];
+//for (i=0; i <tdContentsArray.length; i++) 
+    {arrayOfDivTypes.push(tdContentsArray[1][1]);}
+
+// check if object is a array
+//if( Object.prototype.toString.call( someVar ) === '[object Array]' ) {
+//    alert( 'Array!' );
+//}
